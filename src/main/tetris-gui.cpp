cdde29eb9adc300ee1da2fe1699db6b28b5218f3
zfazek <ogroa@gmail.com>
2020-01-25 08:53:37 +0100

    add desktop version

---
+++ src/main/tetris-gui.cpp
@@ -0,0 +1,30 @@
+#include "table.hpp"
+#include "ui.hpp"
+#include "ui_sdl.hpp"
+
+#include <chrono>
+#include <thread>
+
+int main() {
+    Table table;
+    UiPtr gui = std::make_unique<UiSdl>();
+
+    std::chrono::system_clock::time_point prev_time = std::chrono::system_clock::now();
+    while (gui->game_state != GameState::QUIT) {
+        gui->clear_background();
+        gui->draw(table);
+        if (gui->game_state == GameState::GAME) {
+            std::chrono::system_clock::time_point current_time = std::chrono::system_clock::now();
+            const float elapsed_sec =
+                std::chrono::duration<float, std::milli>(current_time - prev_time).count() / 1000.0f;
+            prev_time = current_time;
+            const bool end = table.update(elapsed_sec);
+            if (end) {
+                gui->game_state = GameState::WAIT_FOR_RESTART;
+            }
+        }
+        gui->update(table);
+    }
+    puts("");
+}
+
